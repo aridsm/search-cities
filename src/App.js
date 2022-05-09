a8d535e01e63fc400cde1b3d30ae9cfd3f21f403
aridsm <aniemorelanie@gmail.com>
2022-05-09 13:09:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App;
