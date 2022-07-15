ee5003a83561021b13c83dbc14c4439a263f0941
aridsm <aniemorelanie@gmail.com>
2022-07-15 13:32:07 -0300

    teste rotas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PagInicial from './Rotas/PagInicial';
 import Header from './Rotas/Header'
 import Footer from './Rotas/Footer'
@@ -6,22 +6,31 @@ import PagListaCidades from './Rotas/PagListaCidades'
 import Cidades from './Rotas/Cidades'
 import PagCidades from './Rotas/PagCidades';
 import Erro404 from './Rotas/Erro404';
+import Rotas from './Rotas';
 
-
-const App = () => {
-  return (
-    <BrowserRouter>
-    <Header />
-    <Routes>
+/*
       <Route path='/search-cities' element={<PagInicial />} />
       <Route path='/search-cities/cidades' element={<Cidades />}>
         <Route path='' element={<PagListaCidades />} />
         <Route path=':id' element={<PagCidades />} />
         <Route path=':id/*' element={<Erro404 />} />
-      </Route>
-      <Route path='/search-cities/*' element={<Erro404 />} />
-    </Routes>
-    <Footer />
+      </Route> */
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/search-cities' element={<Rotas />}>
+          <Route path='/search-cities' element={<PagInicial />} />
+          <Route path='/search-cities/cidades' element={<Cidades />}>
+            <Route path='' element={<PagListaCidades />} />
+            <Route path=':id' element={<PagCidades />} />
+            <Route path=':id/*' element={<Erro404 />} />
+          </Route>
+          <Route path='/search-cities/*' element={<Erro404 />} />
+        </Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
